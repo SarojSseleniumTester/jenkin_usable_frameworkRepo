542a4b391efb4005d54c6a366cba53c9be916723
SarojSseleniumTester <sarojkdash7@gmail.com>
2025-07-29 12:42:13 +0530

    Pushed Two New Class

---
+++ JENKIN_GITHUB_COLLABORATION/src/test/java/Pac1/DeleteContact.java
@@ -0,0 +1,13 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class DeleteContact 
+{
+	@Test
+	public void sample_delete()
+	{
+		System.out.println("execute DeleteContact");
+	}
+
+}
---
+++ JENKIN_GITHUB_COLLABORATION/src/test/java/Pac2/Delete_Org.java
@@ -0,0 +1,13 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class Delete_Org 
+{
+	@Test
+	public void sample_org_delete()
+	{
+		System.out.println("execute sample_org_delete");
+	}
+
+}
