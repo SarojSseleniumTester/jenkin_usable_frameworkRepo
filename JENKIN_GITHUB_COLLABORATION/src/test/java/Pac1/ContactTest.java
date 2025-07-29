7b890a2920431ffdca3c6424d924ceaffc0b4ffa
SarojSseleniumTester <sarojkdash7@gmail.com>
2025-07-29 11:36:10 +0530

    First Push

---
+++ JENKIN_GITHUB_COLLABORATION/src/test/java/Pac1/ContactTest.java
@@ -0,0 +1,30 @@
+package Pac1;
+
+import org.testng.annotations.Test;
+
+public class ContactTest 
+{
+	@Test
+	public void CreateContact()
+	{
+		System.out.println("CreateContact");
+		String URL=System.getProperty("url");
+		String BROWSER=System.getProperty("browser","chrome");
+		String USERNAME=System.getProperty("username");
+		String PASSWORD=System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+
+
+		
+	}
+	@Test
+	public void ModifyContact()
+	{
+		System.out.println("ModifyContaxt");
+	}
+
+}
---
+++ JENKIN_GITHUB_COLLABORATION/src/test/java/Pac2/OrgTest.java
@@ -0,0 +1,24 @@
+package Pac2;
+
+import org.testng.annotations.Test;
+
+public class OrgTest 
+{
+	@Test
+	public void CreateContact()
+	{
+		System.out.println("CreateOrg");
+	}
+	@Test
+	public void ModifyOrg()
+	{
+		System.out.println("ModifyOrg");
+
+	}
+	
+//	 <properties>
+//	  <maven.compiler.target>1.11</maven.compiler.target>
+//	  <maven.compiler.source>1.11</maven.compiler.source>
+//	  </properties>
+
+}
